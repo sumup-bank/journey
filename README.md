244a59456d3d3177e7b8a78d8f4a93979d6b92be
Everton Ribeiro <nuxlli@gmail.com>
2020-02-29 20:40:08 -0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# journey
+# Journey
+
 A simple sagas pattern implementation in Elixir
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `journey` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:journey, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/journey](https://hexdocs.pm/journey).
+
---
+++ lib/journey.ex
@@ -0,0 +1,145 @@
+defmodule Journey.Step do
+  defstruct [:spec, :transaction, :compensation]
+end
+
+defmodule Journey do
+  defstruct data: nil, steps: [], state: :new, result: nil
+
+  alias Journey.Step
+
+  def new() do
+    %__MODULE__{}
+  end
+
+  def set_data(%__MODULE__{} = journey, data) do
+    %__MODULE__{journey | data: data}
+  end
+
+  def run(%__MODULE__{} = journey, spec, args \\ []) do
+    add_step(journey, spec, args, :sync)
+  end
+
+  def run_async(%__MODULE__{} = journey, spec, args \\ []) do
+    add_step(journey, spec, args, :async)
+  end
+
+  def await(%__MODULE__{steps: steps} = journey) do
+    journey
+    |> update_steps(Enum.map(steps, &await(&1)))
+    |> run_compensation?()
+  end
+
+  def await(%Step{transaction: {func, %Task{} = task}} = step) do
+    %Step{step | transaction: {func, Task.await(task)}}
+  end
+
+  def await(step), do: step
+
+  def finally(%__MODULE__{} = journey, func) do
+    func.(await(journey))
+  end
+
+  def finally(%__MODULE__{} = journey) do
+    finally(journey, fn %{result: result} -> result end)
+  end
+
+  defp add_step(%__MODULE__{} = journey, spec, args, :sync = type) do
+    journey
+    |> await()
+    |> mk_step(spec, args, type)
+    |> await()
+  end
+
+  defp add_step(%__MODULE__{} = journey, spec, args, type) do
+    mk_step(journey, spec, args, type)
+  end
+
+  defp mk_step(%__MODULE__{state: :failed} = journey, _, _, _), do: journey
+
+  defp mk_step(%__MODULE__{} = journey, spec, args, type) do
+    {transaction, compensation} = get_funcs(spec, args)
+
+    step = %Step{
+      spec: {spec, args},
+      compensation: {compensation, nil},
+      transaction: {transaction, call(transaction, journey, type)}
+    }
+
+    update_steps(journey, journey.steps ++ [step])
+  end
+
+  defguardp is_valid_function(func) when is_function(func, 0) or is_function(func, 1)
+  defguardp is_ok(result) when result == :ok or elem(result, 0) == :ok
+
+  defp call(func, journey, :async) do
+    call(fn -> Task.async(func) end, journey, :sync)
+  end
+
+  defp call(func, _, _) when is_function(func, 0), do: func.()
+
+  defp call(func, %__MODULE__{} = journey, _) when is_function(func, 1) do
+    func.(journey)
+  end
+
+  defp get_funcs({module, function_name}, args) do
+    apply(module, function_name, args) |> extract_funcs()
+  end
+
+  defp get_funcs(func, _) when is_function(func) do
+    func.() |> extract_funcs()
+  end
+
+  defp extract_funcs({transaction, compensation} = funcs)
+       when is_valid_function(transaction) and is_valid_function(compensation),
+       do: funcs
+
+  defp extract_funcs(transaction) when is_valid_function(transaction), do: {transaction, nil}
+
+  defp run_compensation?(%__MODULE__{steps: steps} = journey) do
+    with true <-
+           Enum.any?(steps, fn
+             %Step{transaction: {_, result}} when is_ok(result) -> false
+             _ -> true
+           end) do
+      rollback(%__MODULE__{journey | state: :failed})
+    else
+      _ -> journey
+    end
+  end
+
+  defp rollback(%__MODULE__{steps: steps} = journey) do
+    steps =
+      steps
+      |> Enum.reverse()
+      |> Enum.map(&call_compensation(&1, journey))
+      |> Enum.reverse()
+
+    %__MODULE__{journey | steps: steps}
+  end
+
+  defp call_compensation(
+         %Step{compensation: {func, _}, transaction: {_, result}} = step,
+         journey
+       )
+       when is_function(func) and is_ok(result) do
+    %Step{step | compensation: {func, call(func, journey, :sync)}}
+  end
+
+  defp call_compensation(step, _journey), do: step
+
+  defp update_steps(%__MODULE__{} = journey, []), do: journey
+
+  defp update_steps(%__MODULE__{} = journey, steps) do
+    with true <-
+           Enum.any?(steps, fn
+             %Step{transaction: {_, %Task{}}} -> true
+             _ -> false
+           end) do
+      %__MODULE__{journey | steps: steps, state: :running, result: nil}
+    else
+      _ ->
+        %Step{transaction: {_, result}} = List.last(steps)
+        %__MODULE__{journey | steps: steps, state: :done, result: result}
+    end
+  end
+end
